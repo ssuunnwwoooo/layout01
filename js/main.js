5b7d3a21028773aa8e8b45e7be0ac0ebd8969475
sunwoo <133613205+ssuunnwwoooo@users.noreply.github.com>
2023-05-31 18:07:21 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,75 @@
+$(function () {
+
+    $('.main_slide').on('init afterChange', function (e, s, c) {
+        const current = $('.main_slide .slick-current');
+        current.addClass('on').siblings().removeClass('on');
+
+        $('.main_visual .slide_num span').text(c ? (c + 1) : 1);
+        $('.main_visual .slide_num strong').text(s.slideCount);
+
+        console.log(s.slideCount)
+
+        $('.main_visual .menu li').eq(0).addClass('on');
+        $('.main_visual .menu li').eq(c).addClass('on')
+            .siblings().removeClass('on');
+    });
+
+    $('.main_slide').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 1000,
+        pauseOnHover: false,
+        fade: true,
+    });
+
+
+    $('.main_visual .arrows .left').on('click', function () {
+        $('.main_slide').slick('slickPrev')
+    });
+    $('.main_visual .arrows .right').on('click', function () {
+        $('.main_slide').slick('slickNext')
+    });
+
+    $('.main_visual .menu li').on('click', function (e) {
+        e.preventDefault();
+        const idx = $(this).index(); // 0,1,2
+        $('.main_slide').slick('slickGoTo', idx);
+    });
+
+
+
+    $('.product_slide').slick({
+        slidesToShow: 3,
+        asNavFor: ".pic_slide",
+        focusOnSelect: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+    $('.pic_slide').slick({
+        vertical: true,
+        asNavFor: ".product_slide",
+
+    });
+
+
+    $('.to_top').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 600)
+    });
+
+    $('.scr').on('click', function (e) {
+        e.preventDefault();
+        const st = $(this.hash).offset().top;
+        console.log(st);
+        $('html, body').animate({ scrollTop: st }, 600)
+    })
+
+
+
+})
